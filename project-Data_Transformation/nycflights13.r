64fd062a7249e4db162cde0df5618fcc0739e677
Kritanol <120021948+Kritanol@users.noreply.github.com>
2022-12-08 09:54:59 +0700

    Create data transformation project by R

---
+++ project-Data_Transformation/nycflights13.r
@@ -0,0 +1,15 @@
+# Install packages
+install.packages(c("dplyr", "tidyr", "tidyverse","tibbletime"))
+
+# Install libraries
+library("dplyr")
+library("tidyr")
+library("tidyverse")
+library("tibbletime")
+
+# Install nycflights13 dataset
+install.packages("nycflights13")
+library("nycflights13")
+
+# Exploring flights table
+glimpse(flights)
